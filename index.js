ef876d5a233c11830688f667b225121c656b3929
Shannon Macias <shannonkalei@gmail.com>
2022-04-12 22:16:20 -0700

    Handle socket closing and reconnection

--- index.js
+++ index.js
@@ -25,201 +25,238 @@ const token = process.env.BOT_ACCESS_TOKEN;
 const WS_URL = 'wss://api.guilded.gg/v1/websocket';
 const GUILDED_BASE_URL = 'https://www.guilded.gg/api/v1';
 
-const socket = new WebSocket(WS_URL, {
-  headers: {
-    Authorization: `Bearer ${token}`
-  },
-});
+let reconnectTimer = null;
 
-console.log(`  Websocket connecting to ${WS_URL}...`);
+function connect() {
+    const socket = new WebSocket(WS_URL, {
+      headers: {
+        Authorization: `Bearer ${token}`
+      },
+    });
 
-socket.on('open', function() {
-  console.log('  Connected to Guilded!');
-  console.log(`  Using base URL: ${GUILDED_BASE_URL}`);
-});
+    console.log(`  Websocket connecting to ${WS_URL}...`);
 
-// Keep server awake
-setInterval(() => {
-  http.get("http://guilded-dice-bot.herokuapp.com/");
-},600000);
+    socket.on('open', function() {
+        stopOtherReconnects();
+      console.log('  Connected to Guilded!');
+      console.log(`  Using base URL: ${GUILDED_BASE_URL}`);
+    });
 
-// Dice rolling options
-const HELP_MESSAGES = [
-  'Hello there! I am a dice rolling bot. 🎲 \n' +  
-  'I can roll dice for you!  Just let me know which dice to roll by using **!d** followed by the number of sides the dice should have. \n',
+    // Keep server awake
+    setInterval(() => {
+      http.get("http://guilded-dice-bot.herokuapp.com/");
+    },600000);
 
-  'Dice bot here! 👋  Need some help?'
-];
+    // Dice rolling options
+    const HELP_MESSAGES = [
+      'Hello there! I am a dice rolling bot. 🎲 \n' +  
+      'I can roll dice for you!  Just let me know which dice to roll by using **!d** followed by the number of sides the dice should have. \n',
 
-const COMMON_CMDS =
-  '\nHere are some common commands you can use: \n' +
-  '**!d6** - roll a 6 sided dice \n' +
-  '**!d20** - roll a 20 sided dice \n' +
-  '**!d100** - roll a 100 sided dice \n' +
-  '**!3d6** - roll three 6 sided dice (I can only roll up to 10 dice at a time)';
+      'Dice bot here! 👋  Need some help?'
+    ];
 
-const D_ZERO_MESSAGES = [
-  `Rolling a d0... uhhh... wait how do I roll this thing? 🤔`,
-  'You want me to roll a how many sided dice?  **Zero**?  Show me how to do this.',
-  'Signs point to **zero**.',
-  'Anyone have a spare d0 I can borrow? ...asking for a friend. 👀',
-  'Did you mean to roll a d10?',
-  'Did you mean to roll a d20?'
-];
+    const COMMON_CMDS =
+      '\nHere are some common commands you can use: \n' +
+      '**!d6** - roll a 6 sided dice \n' +
+      '**!d20** - roll a 20 sided dice \n' +
+      '**!d100** - roll a 100 sided dice \n' +
+      '**!3d6** - roll three 6 sided dice (I can only roll up to 10 dice at a time)';
 
-// Web socket that listens for new messages
-socket.on('message', function incoming(data) {
-  const {t: eventType, d: eventData} = JSON.parse(data);
+    const D_ZERO_MESSAGES = [
+      `Rolling a d0... uhhh... wait how do I roll this thing? 🤔`,
+      'You want me to roll a how many sided dice?  **Zero**?  Show me how to do this.',
+      'Signs point to **zero**.',
+      'Anyone have a spare d0 I can borrow? ...asking for a friend. 👀',
+      'Did you mean to roll a d10?',
+      'Did you mean to roll a d20?'
+    ];
 
-  if (eventType === 'ChatMessageCreated') {
-    const {message: {id: messageId, content, channelId}} = eventData;
-    let input = content;
-    let messageContent = input.toLowerCase();
-    const regex = /,/ig;
-    input = input.replace(regex, '');
-    messageContent = messageContent.replace(regex, '');
-    const replyMessageIds = [messageId];
+    // Web socket that listens for new messages
+    socket.on('message', function incoming(data) {
+      const {t: eventType, d: eventData} = JSON.parse(data);
 
-    // Check for ! command... should we add other commands aside from !d commands?
-    if (messageContent.indexOf('!') === 0) {
-      if (messageContent === '!dhelp' || messageContent === '!d?') {
-        const index = Math.floor(Math.random() * HELP_MESSAGES.length);
-        const helpMessage = HELP_MESSAGES[index].concat(COMMON_CMDS);
+      if (eventType === 'ChatMessageCreated') {
+        const {message: {id: messageId, content, channelId}} = eventData;
+        let input = content;
+        let messageContent = input.toLowerCase();
+        const regex = /,/ig;
+        input = input.replace(regex, '');
+        messageContent = messageContent.replace(regex, '');
+        const replyMessageIds = [messageId];
 
-        // Creates a new channel message
-        fetch(`${GUILDED_BASE_URL}/channels/${channelId}/messages`, {
-          method: 'POST',
-          body: JSON.stringify({
-            "content": helpMessage,
-            "replyMessageIds" : replyMessageIds
-          }),
-          headers: {
-            'Authorization': `Bearer ${token}`,
-            'Accept': 'application/json',
-            'Content-Type': 'application/json'
-          }
-        })
-          .then((response) => {
-            // console.log('response: ', response);
-          });
-      } if (messageContent === '!d0') {
-        const index = Math.floor(Math.random() * D_ZERO_MESSAGES.length);
+        // Check for ! command... should we add other commands aside from !d commands?
+        if (messageContent.indexOf('!') === 0) {
+          if (messageContent === '!dhelp' || messageContent === '!d?') {
+            const index = Math.floor(Math.random() * HELP_MESSAGES.length);
+            const helpMessage = HELP_MESSAGES[index].concat(COMMON_CMDS);
 
-        // Creates a new channel message
-        fetch(`${GUILDED_BASE_URL}/channels/${channelId}/messages`, {
-          method: 'POST',
-          body: JSON.stringify({
-            "content": `${D_ZERO_MESSAGES[index]}`,
-            "replyMessageIds" : replyMessageIds
-          }),
-          headers: {
-            'Authorization': `Bearer ${token}`,
-            'Accept': 'application/json',
-            'Content-Type': 'application/json'
-          }
-        });
-      } else if (messageContent.indexOf('d') === 1 && input.length > 2) {
-        let diceType = input.slice(1, input.length);
-        let diceMax = messageContent.slice(2, messageContent.length);
+            // Creates a new channel message
+            fetch(`${GUILDED_BASE_URL}/channels/${channelId}/messages`, {
+              method: 'POST',
+              body: JSON.stringify({
+                "content": helpMessage,
+                "replyMessageIds" : replyMessageIds
+              }),
+              headers: {
+                'Authorization': `Bearer ${token}`,
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+              }
+            })
+              .then((response) => {
+                // console.log('response: ', response);
+              });
+          } if (messageContent === '!d0') {
+            const index = Math.floor(Math.random() * D_ZERO_MESSAGES.length);
 
-        if (diceMax.toLowerCase() == 'pi') {
-          diceMax = '3.14';
-          diceType = '3.14';
-        }
+            // Creates a new channel message
+            fetch(`${GUILDED_BASE_URL}/channels/${channelId}/messages`, {
+              method: 'POST',
+              body: JSON.stringify({
+                "content": `${D_ZERO_MESSAGES[index]}`,
+                "replyMessageIds" : replyMessageIds
+              }),
+              headers: {
+                'Authorization': `Bearer ${token}`,
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+              }
+            });
+          } else if (messageContent.indexOf('d') === 1 && input.length > 2) {
+            let diceType = input.slice(1, input.length);
+            let diceMax = messageContent.slice(2, messageContent.length);
 
-        let diceRoll;
-        // Negative numbers we should round down, positive numbers round up
-        if (messageContent.indexOf('d-') === 1) {
-          diceRoll = Math.floor(Math.random() * diceMax);
-        } else {
-          diceRoll = Math.ceil(Math.random() * diceMax);
-        }
+            if (diceMax.toLowerCase() == 'pi') {
+              diceMax = '3.14';
+              diceType = '3.14';
+            }
 
-        if (diceRoll && !isNaN(diceRoll)) {
-          // Default message styling
-          let diceRollMessage = `Rolling 1 ${diceType}`;
-          let resultMessage = ` You rolled a **${diceRoll}**!`;
-          if (diceMax.length > 3 && diceMax == Math.PI.toFixed(diceMax.length - 1).toString().slice(0, -1)) {
-            diceRollMessage = `🥧 Rolling 1 ${diceType}`;
-            resultMessage = `You rolled a **${diceRoll}**! 🥧`;
-          }
-
-          const messageArray = ['.', '.', '.', resultMessage];
-
-          // Post new channel message
-          createDiceRollMessage(diceRollMessage, messageArray, channelId, replyMessageIds, token);
-        }
-      } else if (
-          (messageContent.indexOf('d') > 1)
-          && messageContent.length > 3
-        ) {
-          let inputDiceAmount = messageContent.slice(1, messageContent.indexOf('d'));
-          if (inputDiceAmount.toLowerCase() == 'pi') {
-            inputDiceAmount = '3.14';
-          }
-          let diceAmount = parseInt(inputDiceAmount);
-
-        if (diceAmount && !isNaN(diceAmount)) {
-          let diceType = input.slice(messageContent.indexOf('d'), input.length);
-          let diceMax = messageContent.slice((messageContent.indexOf('d') + 1), messageContent.length);
-          if (diceMax.toLowerCase() == 'pi') {
-            diceMax = '3.14';
-            diceType = '3.14';
-          }
-
-          let diceRollMessage = `Rolling ${diceAmount} ${diceType}s`;
-
-          if ((diceMax.length > 3 && diceMax == Math.PI.toFixed(diceMax.length - 1).toString().slice(0, -1)) || (inputDiceAmount.length > 3 && inputDiceAmount == Math.PI.toFixed(inputDiceAmount.length - 1).toString().slice(0, -1))) {
-            diceRollMessage = `🥧 Rolling ${inputDiceAmount} ${diceType}s`;
-          }
-          if (diceAmount === 1) {
-            diceRollMessage = diceRollMessage.slice(0, -1);
-          }
-
-         
-
-          // Only allow max of 10 dice and only positive numbers
-          if (diceAmount > 10) {
-            diceRollMessage = `Sorry, I only have 10 magic dice to roll at a time. \n Rolling ${10} ${diceType}s`
-          } else if (diceAmount < -10) {
-            diceRollMessage = `Sorry, I only have -10 magic *negative* dice to roll at a time. \n Rolling ${-10} ${diceType}s`
-          }
-          diceAmount = Math.min(Math.abs(diceAmount), 10);
-          
-          if (diceMax && !isNaN(diceMax)) {
             let diceRoll;
-            let resultMessage = ['You rolled '];
-            for (let i = 0; i < diceAmount; i++) {
-              // Negative numbers we should round down, positive numbers round up
-              if (messageContent.indexOf('d-') != -1) {
-                diceRoll = Math.floor(Math.random() * diceMax);
-              } else {
-                diceRoll = Math.ceil(Math.random() * diceMax);
+            // Negative numbers we should round down, positive numbers round up
+            if (messageContent.indexOf('d-') === 1) {
+              diceRoll = Math.floor(Math.random() * diceMax);
+            } else {
+              diceRoll = Math.ceil(Math.random() * diceMax);
+            }
+
+            if (diceRoll && !isNaN(diceRoll)) {
+              // Default message styling
+              let diceRollMessage = `Rolling 1 ${diceType}`;
+              let resultMessage = ` You rolled a **${diceRoll}**!`;
+              if (diceMax.length > 3 && diceMax == Math.PI.toFixed(diceMax.length - 1).toString().slice(0, -1)) {
+                diceRollMessage = `🥧 Rolling 1 ${diceType}`;
+                resultMessage = `You rolled a **${diceRoll}**! 🥧`;
               }
-              if (i === (diceAmount - 1)) {
-                resultMessage.push(`${diceRoll}!`);
-                if ((diceMax.length > 3 && diceMax == Math.PI.toFixed(diceMax.length - 1).toString().slice(0, -1)) || (inputDiceAmount.length > 3 && inputDiceAmount == Math.PI.toFixed(inputDiceAmount.length - 1).toString().slice(0, -1))) {
-                  resultMessage.push(` 🥧`);
+
+              const messageArray = ['.', '.', '.', resultMessage];
+
+              // Post new channel message
+              createDiceRollMessage(diceRollMessage, messageArray, channelId, replyMessageIds, token);
+            }
+          } else if (
+              (messageContent.indexOf('d') > 1)
+              && messageContent.length > 3
+            ) {
+              let inputDiceAmount = messageContent.slice(1, messageContent.indexOf('d'));
+              if (inputDiceAmount.toLowerCase() == 'pi') {
+                inputDiceAmount = '3.14';
+              }
+              let diceAmount = parseInt(inputDiceAmount);
+
+            if (diceAmount && !isNaN(diceAmount)) {
+              let diceType = input.slice(messageContent.indexOf('d'), input.length);
+              let diceMax = messageContent.slice((messageContent.indexOf('d') + 1), messageContent.length);
+              if (diceMax.toLowerCase() == 'pi') {
+                diceMax = '3.14';
+                diceType = '3.14';
+              }
+
+              let diceRollMessage = `Rolling ${diceAmount} ${diceType}s`;
+
+              if ((diceMax.length > 3 && diceMax == Math.PI.toFixed(diceMax.length - 1).toString().slice(0, -1)) || (inputDiceAmount.length > 3 && inputDiceAmount == Math.PI.toFixed(inputDiceAmount.length - 1).toString().slice(0, -1))) {
+                diceRollMessage = `🥧 Rolling ${inputDiceAmount} ${diceType}s`;
+              }
+              if (diceAmount === 1) {
+                diceRollMessage = diceRollMessage.slice(0, -1);
+              }
+
+             
+
+              // Only allow max of 10 dice and only positive numbers
+              if (diceAmount > 10) {
+                diceRollMessage = `Sorry, I only have 10 magic dice to roll at a time. \n Rolling ${10} ${diceType}s`
+              } else if (diceAmount < -10) {
+                diceRollMessage = `Sorry, I only have -10 magic *negative* dice to roll at a time. \n Rolling ${-10} ${diceType}s`
+              }
+              diceAmount = Math.min(Math.abs(diceAmount), 10);
+              
+              if (diceMax && !isNaN(diceMax)) {
+                let diceRoll;
+                let resultMessage = ['You rolled '];
+                for (let i = 0; i < diceAmount; i++) {
+                  // Negative numbers we should round down, positive numbers round up
+                  if (messageContent.indexOf('d-') != -1) {
+                    diceRoll = Math.floor(Math.random() * diceMax);
+                  } else {
+                    diceRoll = Math.ceil(Math.random() * diceMax);
+                  }
+                  if (i === (diceAmount - 1)) {
+                    resultMessage.push(`${diceRoll}!`);
+                    if ((diceMax.length > 3 && diceMax == Math.PI.toFixed(diceMax.length - 1).toString().slice(0, -1)) || (inputDiceAmount.length > 3 && inputDiceAmount == Math.PI.toFixed(inputDiceAmount.length - 1).toString().slice(0, -1))) {
+                      resultMessage.push(` 🥧`);
+                    }
+                  } else {
+                    resultMessage.push(`${diceRoll} | `);
+                  }
                 }
-              } else {
-                resultMessage.push(`${diceRoll} | `);
+
+                let messageArray = ['.', '.', '.'];
+
+                for (let i = 0; i < resultMessage.length; i++) {
+                  messageArray.push(resultMessage[i]);
+                }
+
+                // Post new channel message
+                createDiceRollMessage(diceRollMessage, messageArray, channelId, replyMessageIds, token);
               }
             }
-
-            let messageArray = ['.', '.', '.'];
-
-            for (let i = 0; i < resultMessage.length; i++) {
-              messageArray.push(resultMessage[i]);
-            }
-
-            // Post new channel message
-            createDiceRollMessage(diceRollMessage, messageArray, channelId, replyMessageIds, token);
           }
         }
       }
-    }
+    });
+
+  socket.on('close', function clear() {
+    socket.terminate();
+    console.log("Socket connection closed.");
+    stopOtherReconnects();
+    reconnectTimer = setTimeout(reconnect, 5000);
+  });
+
+  return socket;
+}
+
+function reconnect() {
+  console.log("Attempting to connect...");
+  stopOtherReconnects();
+  const socket = connect();
+  reconnectTimer = setTimeout(function() {
+    socket.terminate();
+    reconnect();
+  }, 5000);
+}
+
+function stopOtherReconnects() {
+  if (reconnectTimer) {
+    clearTimeout(reconnectTimer);
+    reconnectTimer = null;
   }
-});
+}
+
+reconnect();
+
+/*
+ *  Async channel message requests
+ */
 
 async function createDiceRollMessage(startMessage, messageArray, channelId, replyMessageIds, token) {
   let messageId;
